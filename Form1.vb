54499bae35bdb1620d61082d079af01f877d7a98
Jeffrey Saelee <gwfds123@gmail.com>
2021-03-05 22:12:37 -0800

    added couple timer/keydown

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
 
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
